9e604aaeb4b422c2dadd6661eaefa7ec04b0e728
jayanthchandra <jayanthchandra14@gmail.com>
2023-10-23 01:26:43 +0530

    Binding of Comment Toggle

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -1,34 +1,41 @@
 return {
   {
-      'numToStr/Comment.nvim',
-      opts = {
-          -- add any options here
+    'numToStr/Comment.nvim',
+    opts = {
+      toggler = {
+        line = "<C-/>",
+        block = "<C-?>",
       },
-      lazy = false,
+      opleader = {
+        line = "<C-/>",
+        block = "<C-?>",
+      },
+    },
+    lazy = false,
   },
-   {
-      "m4xshen/autoclose.nvim",
-      opts = {
-         options = {
-            disabled_filetypes = { "text" },
-            disable_when_touch = true,
-            pair_spaces = true,
-         },
-         keys = {
-            ["'"] = {
-               escape = true,
-               close = true,
-               pair = "''",
-               disabled_filetypes = { "markdown" },
-            },
-            ["`"] = { escape = false, close = true, pair = "``" },
-            ["<"] = {
-               escape = false,
-               close = true,
-               pair = "<>",
-               enabled_filetypes = { "rust" },
-            },
-         },
+  {
+    "m4xshen/autoclose.nvim",
+    opts = {
+      options = {
+        disabled_filetypes = { "text" },
+        disable_when_touch = true,
+        pair_spaces = true,
       },
-   }
+      keys = {
+        ["'"] = {
+          escape = true,
+          close = true,
+          pair = "''",
+          disabled_filetypes = { "markdown" },
+        },
+        ["`"] = { escape = false, close = true, pair = "``" },
+        ["<"] = {
+          escape = false,
+          close = true,
+          pair = "<>",
+          enabled_filetypes = { "rust" },
+        },
+      },
+    },
+  }
 }
